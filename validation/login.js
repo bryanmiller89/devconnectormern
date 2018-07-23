59a8eb53e506a16ef9da89a862fa213abff45e77
Bryan Miller <bryanmiller89@gmail.com>
2018-07-23 13:35:03 -0700

    Create validation checks for user's login inputs

---
+++ validation/login.js
@@ -0,0 +1,27 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  // Set data to be an empty string if no name is supplied for Validator
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
