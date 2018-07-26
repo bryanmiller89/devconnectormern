a1b92f9943411c9a427954c2b7742c48069c56b7
Bryan Miller <bryanmiller89@gmail.com>
2018-07-25 18:41:06 -0700

    Fixed typo

--- validation/register.js
+++ validation/register.js
@@ -35,7 +35,7 @@ module.exports = function validateRegisterInput(data) {
   }
 
   if (Validator.isEmpty(data.password2)) {
-    errors.password = 'Confirm Password field is required';
+    errors.password2 = 'Confirm Password field is required';
   }
 
   if (!Validator.equals(data.password, data.password2)) {
