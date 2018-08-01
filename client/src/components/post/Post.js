b13d8340a76854e5b61b61a3feffc811580df830
Bryan Miller <bryanmiller89@gmail.com>
2018-08-01 16:43:18 -0700

    Build out Comment Form component and action

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,8 +3,10 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
+import { POST_LOADING } from '../../actions/types';
 
 class Post extends Component {
   componentDidMount() {
@@ -21,6 +23,7 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </div>
       );
     }
