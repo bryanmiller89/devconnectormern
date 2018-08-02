17bd7f7631be99129e797e53a244834231a1b4ca
Bryan Miller <bryanmiller89@gmail.com>
2018-08-01 17:41:15 -0700

    CommentForm component and Comment display/delete functionality

--- validation/post.js
+++ validation/post.js
@@ -7,12 +7,12 @@ module.exports = function validatePostInput(data) {
   // Set data to be an empty string if no name is supplied for Validator
   data.text = !isEmpty(data.text) ? data.text : '';
 
-  if (Validator.isEmpty(data.text)) {
-    errors.text = 'Text field is required';
-  }
-
   if (!Validator.isLength(data.text, { min: 10, max: 300 })) {
     errors.text = 'Post must be between 10 and 300 characters';
+  }
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = 'Text field is required';
   }
 
   return {
